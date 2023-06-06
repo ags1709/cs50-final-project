16effb6b49f1768941154c9c0ccffd03f40f3993
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-06 17:03:47 +0200

    Setup auth and homepage routes with Blueprints

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SECRET KEY"] = "dev"
+
+    from webapp.auth import auth
+    from webapp.views import views
+
+    app.register_blueprint(auth)
+    app.register_blueprint(views)
 
     return app
---
+++ webapp/auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request, flash, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login")
+def login():
+    return "This is the login page"
+
+@auth.route("/register")
+def route():
+    return "This is the register page"
+
+@auth.route("/logout")
+def logout():
+    return "This will log you out"
