d6e8e6831dd41e8e58d135256957616149f67d19
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-06 21:35:48 +0200

    Change '/' route to render index.html

--- webapp/views.py
+++ webapp/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
+
 @views.route("/")
 def index():
-    return "This is the homepage"
+    return render_template("index.html")
