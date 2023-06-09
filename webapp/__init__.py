68ceca65d599f8d0f450b5364a21c6e9a53db1a2
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-09 21:36:54 +0200

    Add startup of session and configuration of it

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, session
 from .config import secret_key
+from flask_session import Session
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SECRET_KEY"] = secret_key
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
 
     from webapp.auth import auth
     from webapp.views import views
