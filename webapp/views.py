191d4583c6572750abce88d57066a7cc6adc3098
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-09 22:01:03 +0200

    Remove access to homepage unless logged in

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
-
+from .helpers import login_required
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
+@login_required
 def index():
     return render_template("index.html")
