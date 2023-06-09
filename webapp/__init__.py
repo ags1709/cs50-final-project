728990c40ed63501e092e7ad003769a5e894ea36
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-09 15:29:15 +0200

    Move secret_key to config.py for security reasons

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-
+from .config import secret_key
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "dev"
+    app.config["SECRET_KEY"] = secret_key
 
     from webapp.auth import auth
     from webapp.views import views
