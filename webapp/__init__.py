40c05fa39c7f993b68358a08b2719e6a1acc261b
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-09 15:24:58 +0200

    Fix bug with misspelling secret_key

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,12 +3,12 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config["SECRET KEY"] = "dev"
+    app.config["SECRET_KEY"] = "dev"
 
     from webapp.auth import auth
     from webapp.views import views
 
     app.register_blueprint(auth)
     app.register_blueprint(views)
-
+    
     return app
