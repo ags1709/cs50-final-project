05dc668397e2b2ce287463c838f27e29c7971e92
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-09 21:41:16 +0200

    Implement functionality of login

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,14 +1,38 @@
-from flask import Blueprint, render_template, request, flash, redirect
+from flask import Blueprint, render_template, request, flash, redirect, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from .db import select_data, update_db
 from datetime import date, datetime
+from .helpers import login_required
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = select_data("SELECT * FROM users WHERE username = :username", username=username)[0]
+
+        # Validate input
+        if not username:
+            # Throw error
+            flash("Enter Username", category="error")
+        elif not password:
+            # Throw error
+            flash("Enter Password", category="error")
+        elif not user:
+            # Throw error
+            flash("User does not exist", category="error")
+        elif not check_password_hash(password, user["hash"]):
+            # Throw error
+            flash("Incorrect password", category="error")
+        else:
+            session["user_id"] = user["id"]
+            return redirect("/")
+        redirect("/login")
+    else:
+        return render_template("login.html")
 
 
 @auth.route("/register", methods=["GET", "POST"])
@@ -25,7 +49,7 @@ def register():
             # Throw error
             flash("Please enter username", category="error")
         elif select_data(
-            "SELECT * FROM users WHERE USERNAME = :username", username=username
+            "SELECT * FROM users WHERE username = :username", username=username
         ):
             # Throw error
             flash("Username taken", category="error")
