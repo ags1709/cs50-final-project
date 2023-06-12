e6e36a3f2d51b753d2ed477d4540f7a3a756a9f1
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-12 11:29:52 +0200

    Implement basic drawing functionality

--- webapp/static/index_script.js
+++ webapp/static/index_script.js
@@ -11,12 +11,30 @@ canvas.style.height = `${canvasSize}px`
 canvas.style.gridTemplateRows = `repeat(${gridSize}, ${pixelSize}px)`
 canvas.style.gridTemplateColumns = `repeat(${gridSize}, ${pixelSize}px)`
 
-
-
 // Add pixels to canvas
-for (let i = 0; i < gridSize**2; i++) {
-    const pixel = document.createElement("div")
-    pixel.classList.add("pixel");
-    canvas.appendChild(pixel);
-}
+add_pixels()
 
+// Enable coloring pixels
+canvas.addEventListener("mousedown", (e) => {
+    e.preventDefault()
+    if (e.target.classList.contains("pixel")) {
+        e.target.style.backgroundColor = "#000000"
+    }
+})
+// Enable continous painting
+canvas.addEventListener("mouseover", (e) => {
+    e.preventDefault()
+    if (e.buttons === 1 && e.target.classList.contains("pixel")) {
+        e.target.style.backgroundColor = "#000000"
+    }
+})
+
+// Define function to fill canvas with pixels
+function add_pixels() {
+    canvas.replaceChildren();
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        canvas.appendChild(pixel);
+    }
+}
