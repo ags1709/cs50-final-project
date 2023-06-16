df9ec3d558442ef5325cfacdbed27004d94c30c8
Anders Greve Soerensen <anders1709@gmail.com>
2023-06-16 13:54:48 +0200

    Add functionality to load single pixelart into drawing board for editing

--- webapp/static/index_script.js
+++ webapp/static/index_script.js
@@ -1,10 +1,15 @@
 let gridSize = 32;
 let canvasSize = 600;
 let pixelSize = canvasSize / gridSize;
-let pixelData = [] // Array to store pixeldata
+let pixelData = [] // Array to store pixeldata for saving
 
 let canvas = document.querySelector("#canvas");
 let saveButton = document.querySelector("#save-button");
+
+// check for search paramaters
+let urlParams = new URLSearchParams(window.location.search)
+let pixelArtIdToLoad = urlParams.get("pixelArtId")
+
 
 // Define metadata
 let title = "myPixelArt";
@@ -17,6 +22,11 @@ canvas.style.gridTemplateColumns = `repeat(${gridSize}, ${pixelSize}px)`
 
 // Add pixels to canvas
 addPixels()
+
+// if the pixelArtId was present in the URL, we load the pixel art
+if (pixelArtIdToLoad) {
+    loadPixelArt(pixelArtIdToLoad)
+}
 
 saveButton.addEventListener("click", savePixelArt);
 
@@ -79,4 +89,37 @@ function savePixelArt() {
         // Handle errors
         console.error('Error', error);
     });
+}
+
+function loadPixelArt(pixelArtId) {
+    fetch("/api/fetch_data", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(pixelArtId)
+    })
+        .then(response => response.json())
+        .then(responseData => {
+            // Handle data
+            handleLoadData(responseData)
+        })
+}
+
+function handleLoadData(data) {
+    let pixelArtData = JSON.parse(data[0]["pixeldata"])
+    // configure canvas to fit the loaded pixelArt
+    gridSize = data[0]["gridsize"]
+    pixelSize = (canvasSize - 2) / gridSize
+    canvas.style.gridTemplateColumns = `repeat(${gridSize}, ${pixelSize}px)`
+    canvas.style.gridTemplateRows = `repeat(${gridSize}, ${pixelSize}px)`
+    // load the pixel art into the canvas
+    canvas.replaceChildren();
+    for (let i = 0; i < pixelArtData.length; i++) {
+        const pixel = document.createElement("div");
+        pixel.style.backgroundColor = `${pixelArtData[i]}`
+        pixel.style.border = "1px solid black"
+        pixel.classList.add("pixel")
+        canvas.appendChild(pixel);
+    }
 }
